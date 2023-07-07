18afd1a842b391245b7a78014f8db4d2522c19c3
SORQ1NGG <daniil.sork@gmail.com>
2023-07-07 21:11:51 +0300

    chore: update configuration + apollo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss',
         '@nuxt/content',
-        'nuxt-icon'
+        'nuxt-icon',
+        '@nuxtjs/apollo'
     ],
+    runtimeConfig: {
+        githubToken: process.env.GITHUB_TOKEN,
+    },
     content: {
         highlight: {
             theme: 'nord',
             preload: ['ts', 'js', 'css', 'json', 'bash', 'vue']
         }
+    },
+    apollo: {
+        clients: {
+            default: {
+                tokenName: 'github-token',
+                httpEndpoint: 'https://api.github.com/graphql'
+            }
+        },
     },
 })
